7ba8f2054655fab42678a112be3c9dbf6196d8fa
Beletskiy <Alexey1Beletskiy@gmail.com>
2015-11-12 13:32:05 +0200

    12_11

--- src/views/house_Info_briefly.view.js
+++ src/views/house_Info_briefly.view.js
@@ -1,3 +1,24 @@
 var HouseView = Backbone.View.extend({
 
+    tagName: 'li',
+    template: _.template($('#list-part2-template').html()),
+    events: {
+        //'click .house': 'houseView',
+
+    },
+
+    initialize: function () {
+        //this.listenTo(app.Collections.ListOfHouses, 'reset', this.render);
+
+    },
+    render: function () {
+
+        for (var i = 1; i < app.Collections.ListOfHouses.models.length; i++) {
+            this.$el.html(this.template({
+                priceFormatted: app.Collections.ListOfHouses.models[i].attributes.price_formatted,
+                summary: app.Collections.ListOfHouses.models[i].attributes.summary
+            }));
+            $('.house-list').append(this.$el.html());
+        }
+    }
 });
--- src/views/list_of_houses.view.js
+++ src/views/list_of_houses.view.js
@@ -1,9 +1,10 @@
 var ListOfHousesView = Backbone.View.extend({
     el: '#wrapper',
-    template: _.template($('#list-template').html()),
+    template: _.template($('#list-part1-template').html()),
 
     initialize: function () {
         this.listenTo(app.Collections.ListOfHouses, 'reset', this.render);
+        this.listenTo(app.Collections.ListOfHouses, 'reset', this.addAll);
     },
 
     initRender: function  (urlParam) {
@@ -12,24 +13,31 @@ var ListOfHousesView = Backbone.View.extend({
             amountHousesOnThePage: 0,
             amountOfAllHouses: 0 // set spinner
         }));
-        $(".spinner-loader").show();
+        $("#spinner-loader").show();
         app.Collections.ListOfHouses.fetch({
             reset: true,
             error: function (collection, response, options) {
                 console.log('error');
+                this.renderError();
             }
         });
+       // console.log(app.Collections.ListOfHouses);
     },
 
     renderError: function  () {
-// show error
+          // show error
     },
 
     render: function () {
         console.log('render');
+        var totalResults = app.Collections.ListOfHouses.models[0].attributes.total_results;
         this.$el.html(this.template({
             amountHousesOnThePage: 20,
-            amountOfAllHouses: this.amountOfAllHouses // initialise must be before render...
+            amountOfAllHouses: totalResults
         }));
+    },
+
+    addAll: function () {
+        $('.house-list').append(app.Views.houseinfoBriefly.render());
     }
 });
